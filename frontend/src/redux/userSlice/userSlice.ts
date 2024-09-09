e1e1ce6fe2630fc7a452cc885fa41d0c75d09b93
Artiquee <amex7708@gmail.com>
2024-09-09 21:45:16 +0300

    feat(userSlice): add extraReducers

--- frontend/src/redux/userSlice/userSlice.ts
+++ frontend/src/redux/userSlice/userSlice.ts
@@ -1,22 +1,39 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, ActionReducerMapBuilder } from '@reduxjs/toolkit';
 import { User, RegisterRequest, LoginRequest } from "../../common/types/User/user.types.ts";
+import { onLogin } from './userThunk.ts';
+import { DataStatus } from "../../common/enums/app/DataStatus.ts";
 
 interface UserState {
     user: User | RegisterRequest | LoginRequest | null ,
-    status: 'idle' | 'loading' | 'succeeded' | 'failed';  // Possible statuses
+    status: DataStatus;  // Possible statuses
     error: string | null;  // Error message or null
 }
 
 const initialState: UserState = {
     user: null,
-    status: 'idle',
+    status: DataStatus.Idle,
     error: null
 }
 
 export const userSlice = createSlice({
-    name: "user",
+    name: 'user',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder: ActionReducerMapBuilder<UserState>) => {
+        builder
+            .addCase(onLogin.pending, (state: UserState) => {
+                state.status = DataStatus.Pending;
+            })
+            .addCase(onLogin.fulfilled, (state: UserState, action: PayloadAction<User>) => {
+                state.status = DataStatus.Success;
+                state.user = action.payload;
+            })
+            .addCase(onLogin.rejected, (state: UserState, action: PayloadAction<string | null>) => {
+                state.status = DataStatus.Error;
+                state.error = action.payload;  // Handle error message
+            });
+    },
 });
 
+export const selectUser = state => state.user.user;
 export const userReducers = userSlice.reducer;
