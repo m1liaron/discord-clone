a18dd3e1c2f23bf227ec3552042db450f62b470f
Vlad <machinker34@gmail.com>
2024-09-16 22:04:30 +0300

    feat: create file for routes pathes

--- backend/src/helpers/registerRoutes.ts
+++ backend/src/helpers/registerRoutes.ts
@@ -1,8 +1,9 @@
 import {Express} from "express";
 import { authRoute } from "../routes/routes";
+import { AppPath } from "../common/app/AppPath";
 
 const registerRoutes = (app: Express) => {
-    app.use('/auth', authRoute);
+    app.use(AppPath.Auth, authRoute);
 };
 
 export { registerRoutes };
