3acc0204b173124240cdf3db35c681a532a0a8f6
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-09-21 17:15:25 +0300

    fix: authentication errors, userSlice types errors
    Took 9 minutes

--- frontend/src/redux/userSlice/userSlice.ts
+++ frontend/src/redux/userSlice/userSlice.ts
@@ -1,11 +1,11 @@
 import { createSlice, PayloadAction, ActionReducerMapBuilder } from '@reduxjs/toolkit';
-import { User, RegisterRequest, LoginRequest } from "../../common/types/User/user.types.ts";
+import {User, LoginResponse} from "../../common/types/User/user.types.ts";
 import { onLogin, onRegister } from './userThunk.ts';
 import { DataStatus } from "../../common/enums/app/DataStatus.ts";
 import { RootState } from '../store.ts';
 
 interface UserState {
-    user: User | RegisterRequest | LoginRequest | null ,
+    user: User | LoginResponse | null ,
     status: DataStatus;  // Possible statuses
     error: string | null;  // Error message or null
 }
@@ -25,25 +25,25 @@ export const userSlice = createSlice({
             .addCase(onLogin.pending, (state: UserState) => {
                 state.status = DataStatus.Pending;
             })
-            .addCase(onLogin.fulfilled, (state: UserState, action: PayloadAction<User>) => {
+            .addCase(onLogin.fulfilled, (state: UserState, action: PayloadAction<LoginResponse>) => {
                 state.status = DataStatus.Success;
                 state.user = action.payload;
             })
             .addCase(onLogin.rejected, (state: UserState, action: PayloadAction<string | null>) => {
                 state.status = DataStatus.Error;
-                state.error = action.payload;  // Handle error message
+                state.error = action.payload;
             })
 
             .addCase(onRegister.pending, (state: UserState) => {
                 state.status = DataStatus.Pending;
             })
-            .addCase(onRegister.fulfilled, (state: UserState, action: PayloadAction<User>) => {
+            .addCase(onRegister.fulfilled, (state: UserState, action: PayloadAction<LoginResponse>) => {
                 state.status = DataStatus.Success;
                 state.user = action.payload;
             })
             .addCase(onRegister.rejected, (state: UserState, action: PayloadAction<string | null>) => {
                 state.status = DataStatus.Error;
-                state.error = action.payload;  // Handle error message
+                state.error = action.payload;
             });
     },
 });
