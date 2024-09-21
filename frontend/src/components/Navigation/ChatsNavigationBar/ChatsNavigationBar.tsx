59cd2173efc2241b2c0fa545077bfd3232db97eb
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-09-21 17:01:54 +0300

    feat: create authorization function
    Took 37 minutes

--- frontend/src/components/Navigation/ChatsNavigationBar/ChatsNavigationBar.tsx
+++ frontend/src/components/Navigation/ChatsNavigationBar/ChatsNavigationBar.tsx
@@ -1,24 +1,21 @@
-import * as React from "react";
-import {NavLink} from "react-router-dom";
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const ChatsNavigationBar: React.FC = () => {
-
-    const users = [
-        {
-            id: 'user-1',
-            name: 'Beaver',
-            image: 'https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png'
-        }
-    ]
-
     return (
-        <div>
-            {users.map((user) => (
-                <NavLink to={`${user.id}`}>
-                    <img src={user.image}/>
-                    <h3>{user.name}</h3>
-                </NavLink>
-            ))}
+        <div className='navBar'>
+            <div className='stocks'>
+                <Link to="/">Home</Link>
+            </div>
+            <div className='favourites'>
+                <Link to="/favourites">Favourites</Link>
+            </div>
+            <div className='cart'>
+                <Link to="/cart">Cart</Link>
+            </div>
+            <div className='channels'>
+                <Link to="/channels/@me">Channels</Link>
+            </div>
         </div>
     );
 }
