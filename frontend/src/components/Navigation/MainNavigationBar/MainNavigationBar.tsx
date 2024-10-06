ad1ad571bfb49a67b06b338ab37d4b5c4b16aa57
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-06 14:54:06 +0300

    feat: add MainNavigationBar
    Took 12 minutes

---
+++ frontend/src/components/Navigation/MainNavigationBar/MainNavigationBar.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Link} from "react-router-dom";
+import {AppPath} from "../../../common/enums/app/AppPath.ts";
+
+const MainNavigationBar: React.FC = () => (
+    <div className="d-flex flex-column">
+        <ul>
+            <Link to={AppPath.ChannelsMe}>Друзі</Link>
+            <Link to={AppPath.ChannelsMe}>Nitro</Link>
+            <Link to={AppPath.ChannelsMe}>Крамниця</Link>
+        </ul>
+    </div>
+);
+
+export default MainNavigationBar;
